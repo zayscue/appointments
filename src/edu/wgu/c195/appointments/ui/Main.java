b683733cdff92a690f2c79d81e7c5f525bfc1be4
Zackery Ayscue <ztayscue@Zackerys-MacBook-Pro.local>
2019-01-21 14:14:42 -0500

    Added support for localization.

--- src/edu/wgu/c195/appointments/ui/Main.java
+++ src/edu/wgu/c195/appointments/ui/Main.java
@@ -8,18 +8,26 @@ import edu.wgu.c195.appointments.persistence.repositories.IRepository;
 import edu.wgu.c195.appointments.persistence.repositories.IncrementTypeRepository;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
 import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
+        Locale deLocale = new Locale("de");
+        ResourceBundle labels = ResourceBundle.getBundle("LabelsBundle", deLocale);
+        String s2 = labels.getString("s2");
+        System.out.println(s2);
         Connection connection = ConnectionFactory.getConnection();
         IncrementTypeRepository repository = new IncrementTypeRepository();
         UserManager userManager = new UserManager();
         try {
             User user = userManager.findByUserName("zayscue");
+            User sameUser = userManager.findById(user.getUserId());
             List<IncrementType> incrementTypes = repository.getAll()
                     .filter(x -> x.getIncrementTypeId() > 1)
                     .collect(Collectors.toList());
