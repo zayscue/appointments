c1e1bd936ff6759344fa2a21f65cc22e115da7c5
Zackery Ayscue <ztayscue@Zackerys-MacBook-Pro.local>
2019-01-22 01:18:58 -0500

    Added more ui elements and built out login page.

--- src/edu/wgu/c195/appointments/ui/login/LoginController.java
+++ src/edu/wgu/c195/appointments/ui/login/LoginController.java
@@ -1,4 +1,15 @@
 package edu.wgu.c195.appointments.ui.login;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+
+    public PasswordField passwordField;
+    public TextField userNameField;
+
+    public void handleSubmitButtonAction(ActionEvent actionEvent) {
+        
+    }
 }
